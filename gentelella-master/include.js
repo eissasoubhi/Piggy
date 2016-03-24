fdf106719fd7554bdf25448bf8f099eca52cba07
netfox01 <eissa.soubhi@gmail.com>
2016-03-24 21:16:46 +0100

    second version without jQuery

--- gentelella-master/include.js
+++ gentelella-master/include.js
@@ -1,38 +1,45 @@
-jQuery(document).ready(function($)
+ready(function()
 {
-    var html_file, comment, filename, $partial;
+    var html_file, comment, filename;
 
-    $('include').each(function(index, el)
+    var include_elements = document.querySelectorAll('include');
+
+    Array.prototype.forEach.call(include_elements, function(el, i)
     {
-        var include = this;
+        var include = el;
 
-        html_file = $(include).attr('src'); // the partial file location
+        html_file = include.getAttribute('src'); // the partial file location
 
         if (typeof html_file !== "undefined" && html_file !== "")
         {
-            $.get( html_file, function(partial)
+            get( html_file, function(partial)
             {
-                $partial = $.parseHTML(partial);
-                $(include).replaceWith($partial); //insert the partial file content
 
-                if ($(include).attr('comment') === "true")
+                if (include.getAttribute('comment') === "true")
                 {   // insert comment
-                    html_file = $(include).attr('src');
+
+                    html_file = include.getAttribute('src');
                     filename = getFileName(html_file);
-                    comment = commentElement(filename)
-                    $(comment).insertBefore($partial[0])
+                    begin_comment = commentElement('begin '+filename)
+                    end_comment = commentElement('end '+filename, 10)
+
+                    partial = begin_comment + partial +end_comment;
                 };
-            })
-            .fail(function()
+
+                include.outerHTML = partial;  //insert the partial file content
+            },
+            function()
             {
                 console.error("couldn't load " + html_file + " file")
-            })
+            });
         }
         else
         {
             console.error("The html source file must be setted in the src attribute")
         }
     });
+
+    /********************* functions *************************/
 
     function getFileName (path) {
         return path.replace(/.*(\/|\\)/, '');
@@ -59,4 +66,47 @@ jQuery(document).ready(function($)
 
         return comment += " -->";
     }
+
+    // ajax get request
+    function get(url, success, fail)
+    {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+
+        request.onload = function()
+        {
+            if (request.status >= 200 && request.status < 400)
+            {
+                //success !
+                var resp = request.responseText;
+                success(resp);
+            }
+            else
+            {
+            // We reached our target server, but it returned an error
+                fail();
+            }
+        };
+
+        request.onerror = function() {
+          // There was a connection error of some sort
+          fail();
+        };
+
+        request.send();
+    }
+
 });
+
+// document content loaded
+function ready(fn)
+{
+    if (document.readyState != 'loading')
+    {
+        fn();
+    }
+    else
+    {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
