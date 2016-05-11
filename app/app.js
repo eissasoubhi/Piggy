02297039711a3f210306d26cab4f12238e8df444
Tailor Developer <dsi@tailorcorner.fr>
2016-05-11 21:49:24 +0200

    installed NG6-starter

--- app/app.js
+++
@@ -1,6 +0,0 @@
-/*require('./app.autoload.js');
-console.log(anguler)
-*/
-var angular = require('angular');
-// angularModule = angular.module('myApp', []);
-console.log(angular)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-'use strict';
-var webpack = require('webpack'),
-path = require('path');
-
-var APP = __dirname + '/app';
+var path    = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: APP,
-     entry: {
-           app: './app.js'
-    },
-    output: {
-        path: APP,
-        filename: 'bundle.js'
-    }
-}
+  devtool: 'sourcemap',
+  entry: {},
+  module: {
+    loaders: [
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       { test: /\.html$/, loader: 'raw' },
+       { test: /\.styl$/, loader: 'style!css!stylus' },
+       { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  plugins: [
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    // Automatically move all modules defined outside of application directory to vendor bundle.
+    // If you are using more complicated project structure, consider to specify common chunks manually.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
+      }
+    })
+  ]
+};
