4cea1a1a617f2a525d622fd23f63eb453905dbd8
Tailor Developer <dsi@tailorcorner.fr>
2016-06-21 21:18:50 +0200

    trnsactionList page data

--- client/app/components/transactionsList/transactionsList.controller.js
+++ client/app/components/transactionsList/transactionsList.controller.js
@@ -1,6 +1,55 @@
 class TransactionsListController {
   constructor() {
-    this.name = 'transactionsList';
+    this.items_count = 20;
+    this.selected_class = 'even';
+
+    this.items = [
+        {
+            "from": "Bank account",
+            "to": ["Wallet"],
+            "amount": "$150",
+            "scheduled": false,
+            "next_scheduled_tran": false,
+            "created_at": "May 23, 2014 11:47:56 PM",
+            "updated_at": "May 23, 2014 11:47:56 PM",
+        },
+        {
+            "from": "Bank account",
+            "to": ["Home", "House stuff", "House Rent"],
+            "amount": "$530",
+            "scheduled": "Every month",
+            "next_scheduled_tran": "Jan 23, 2014 11:47:56 PM",
+            "created_at": "May 23, 2014 11:47:56 PM",
+            "updated_at": "May 23, 2014 11:47:56 PM",
+        },
+        {
+            "from": "Wallet",
+            "to": ["Home", "Tech stuff", "Phone"],
+            "amount": "$100",
+            "scheduled": false,
+            "next_scheduled_tran": false,
+            "created_at": "May 23, 2014 11:47:56 PM",
+            "updated_at": "May 23, 2014 11:47:56 PM",
+        },
+        {
+            "from": "Bank account",
+            "to": ["Home", "House stuff", "House gas"],
+            "amount": "$10",
+            "scheduled": "Every month",
+            "next_scheduled_tran": "Jan 23, 2014 11:47:56 PM",
+            "created_at": "May 23, 2014 11:47:56 PM",
+            "updated_at": "May 23, 2014 11:47:56 PM",
+        },
+        {
+            "from": "Bank account",
+            "to": ["Home", "House stuff", "House electricity"],
+            "amount": "$15",
+            "scheduled": "Every month",
+            "next_scheduled_tran": "Jan 23, 2014 11:47:56 PM",
+            "created_at": "May 23, 2014 11:47:56 PM",
+            "updated_at": "May 23, 2014 11:47:56 PM",
+        }
+    ]
     this.transactions_list = {
                                 "oLanguage": {
                                     "sSearch": "Search all columns:"
@@ -17,6 +66,21 @@ class TransactionsListController {
                                     "sSwfPath": "app/assets/js/datatables/tools/swf/copy_csv_xls_pdf.swf"
                                 }
                             };
+  }
+
+  toggleClass()
+  {
+    if(this.selected_class == 'even')
+    {
+        this.selected_class = 'odd';
+    }
+    else
+    {
+        this.selected_class = 'even';
+    }
+
+
+    return this.selected_class
   }
 }
 
