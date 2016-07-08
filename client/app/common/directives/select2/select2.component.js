7196afde4696c80419852c7de30be376103937d5
Tailor Developer <dsi@tailorcorner.fr>
2016-07-08 23:28:36 +0200

    set byText or byVal options to select2 directive

--- client/app/common/directives/select2/select2.component.js
+++ client/app/common/directives/select2/select2.component.js
@@ -12,23 +12,48 @@ let select2Component = function ($parse)
                 setTimeout(function () {
                     var model = $parse(attrs.select2);
                     var data = scope.$eval(attrs.options);
+                    var bytext = typeof attrs.bytext == "undefined" ? false : true ;
 
                     $(element).select2(data);
-                    $(element).hide();
+                    // $(element).hide();
                     var _scope = scope;
 
                     if(attrs.select2)
                     {
                         $(element).on('select2:select', function (evt) {
                             scope.$apply(function(){
-                                model.assign(scope.$parent, evt.params.data.text);
+                                if(bytext)
+                                {
+                                    model.assign(scope.$parent, evt.params.data.text);
+                                }
+                                else
+                                {
+                                    model.assign(scope.$parent, evt.params.data.id);
+                                }
+
+                                scope.$parent.refresh_modal = false;
                             });
                         });
 
                         // log('scope', scope)
                         scope.$parent.$watch(model, function(newValue, oldValue, scope) {
-                            $(element).change();
+                            var refresh = typeof scope.refresh_modal == 'undefined' ? true : scope.refresh_modal;
+                            log('refresh', refresh)
+                            if(refresh)
+                            {
+                                var selected_value = newValue;
+                                if(bytext)
+                                {
+                                    selected_value = $(element).find('option').filter(function () {
+                                                            return $(this).html() == newValue
+                                                        }).val();
+                                }
+                                $(element).val(selected_value)
+                                $(element).change();
+                            }
+
                             _scope.selectChange()
+                            scope.refresh_modal = true;
                         });
                     }
 
